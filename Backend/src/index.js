2a1c04069e8a610054af84cb9b3244258e5702dd
Luis Facundo Pinto Lopez <lpintol@fi.uba.ar>
2025-12-06 02:45:35 -0300

    cree el archivo base y el archivo que conecta cn la base de datos

---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { pool } from "./db/db.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.json({ message: "Backend funcionando" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost:" + PORT);
+});
