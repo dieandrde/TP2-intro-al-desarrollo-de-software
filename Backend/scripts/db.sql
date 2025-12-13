ddd343eb0ef30ffac38d3abec27cd047fcc66e89
Alexis <jordanpm30@gmail.com>
2025-12-13 18:47:49 -0300

    modificando el db para que este acorde con el jason

--- Backend/scripts/db.sql
+++ Backend/scripts/db.sql
@@ -6,6 +6,7 @@ CREATE TABLE usuarios (
     telefono VARCHAR(20),
     fecha_registro DATE NOT NULL DEFAULT CURRENT_DATE
 );
+
 CREATE TABLE canchas (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
@@ -24,3 +25,10 @@ CREATE TABLE reservas (
     costo_total DECIMAL(10, 2) NOT NULL, 
     estado VARCHAR(20) NOT NULL DEFAULT
 );
+
+INSERT INTO usuarios (nombre, email, password_hash, telefono, fecha_registro)
+VALUES 
+('Jordan Silva', 'jordan.silva@fmail.com', 'hasheado_jordan_1234', '+54 9 11 5555-1234', '2025-11-11'),
+('Alexis Martinez', 'alexis.m@fmail.com', 'hash_alexis_3456', '+54 9 11 1234-0001', '2025-12-10'),
+('Sofía Torres', 'sofia.t@fmail.com', 'hash_sofia_7890', '+54 9 11 1234-0002', '2025-12-13');
+--no le pongo fecha de registro porque asigna la fecha actual aunque lo podria poner 
