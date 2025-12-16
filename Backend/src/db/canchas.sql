0d25b9a72505604e9e9e0b72d8ae8ec00e282c13
dieandrde <deandrade@fi.uba.ar>
2025-12-16 20:08:30 -0300

    correccion pequeño error de sintaxis

--- Backend/src/db/canchas.sql
+++ Backend/src/db/canchas.sql
@@ -4,5 +4,5 @@ CREATE TABLE canchas (
     tipo varchar(50) not null,
     precio_por_hora decimal(10,2) not null,
     ubicacion varchar(255),
-    capacidad int not null,
+    capacidad int NOT NULL
 );
