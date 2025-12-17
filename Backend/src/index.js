9818b2458644f30537c0991d56a5693b99683d6e
Alexis <jordanpm30@gmail.com>
2025-12-17 20:35:05 -0300

    Conectar todo  y habilitar cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { pool } from "./db/db.js";
+import { crearReserva } from "./controllers/reservas.controller.js"; // Importante el .js
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
 
-// Ruta de prueba
+app.use(cors()); 
+
 app.get("/", (req, res) => {
-  res.json({ message: "Backend funcionando" });
+    res.json({ message: "Backend funcionando" });
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/api/reservas", crearReserva);
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en http://localhost:" + PORT);
+    console.log("Servidor corriendo en http://localhost:" + PORT);
 });
+
