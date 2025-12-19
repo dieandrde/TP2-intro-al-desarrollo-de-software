bfa700120f80e00e31e02238f7a46a5ac6906b66
dieandrde <deandrade@fi.uba.ar>
2025-12-19 03:22:06 -0300

    se agrega ruta para listar todas las canchas disponibles

--- Backend/src/routes/canchas.js
+++ Backend/src/routes/canchas.js
@@ -7,3 +7,19 @@ dotenv.config();
 const router = Router();
 
 export default router;
+
+router.get("/canchas", async (req, res) => {
+    try {
+        const GET_CANCHAS = `
+        SELECT id, nombre, tipo, precio_por_hora, ubicacion, capacidad
+        FROM canchas
+        ORDER BY id ASC;
+        `;
+
+        const resultado = await pool.query(GET_CANCHAS);
+        res.json(resultado.rows);
+    } catch (error) {
+        console.error("error al obtener la cancha:", error);
+        res.status(500).json({ mensaje: "error interno del servidor" });
+    }
+});
